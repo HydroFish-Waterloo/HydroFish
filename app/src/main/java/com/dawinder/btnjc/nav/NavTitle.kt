994f0e2fac08c5790ea150aa5c166039fb8514aa
Isabella Chen <i2chen@uwaterloo.ca>
2024-02-02 12:13:53 -0500

    initial homepage and navigation bar

---
+++ app/src/main/java/com/dawinder/btnjc/nav/NavTitle.kt
@@ -0,0 +1,8 @@
+package com.dawinder.btnjc.nav
+
+object NavTitle {
+    const val HOME = "Home"
+    const val HISTORY = "History"
+    const val ACHIEVEMENTS = "Goals"
+    const val SETTINGS = "Settings"
+}
