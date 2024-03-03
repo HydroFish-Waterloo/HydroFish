dd92c80716d1b72e60dd767205685f65aacd14ea
Bella <isabella.chen@mail.utoronto.ca>
2024-03-03 14:30:49 -0500

    remove unused fish library code

--- app/src/main/java/com/hydrofish/app/HydroFishViewModel.kt
+++ app/src/main/java/com/hydrofish/app/HydroFishViewModel.kt
@@ -16,85 +16,12 @@ class HydroFishViewModel: ViewModel() {
         return List(size) { index -> range.start + step * index }
     }
 
-//    init {
-//
-//        viewModelScope.launch {
-//            launch {
-//                uiState.value.animatableX.animateTo(
-//                    targetValue = 100f,
-//                    animationSpec = tween(
-//                        durationMillis = 3000,
-//                        easing = EaseInOutElastic
-//                    )
-//                )
-//                animatableX.animateTo(
-//                    targetValue = -100f,
-//                    animationSpec = tween(
-//                        durationMillis = 3000,
-//                        easing = EaseInOutElastic
-//                    )
-//                )
-//            }
-//
-//            launch {
-//                delay(2800)
-//                animatableFlip.animateTo(
-//                    targetValue = 180f,
-//                    animationSpec = tween(durationMillis = 200)
-//                )
-//                delay(2800)
-//                animatableFlip.animateTo(
-//                    targetValue = 0f,
-//                    animationSpec = tween(durationMillis = 200)
-//                )
-//            }
-//
-//            launch {
-//                animatableY.animateTo(
-//                    targetValue = 100f,
-//                    animationSpec = tween(durationMillis = 3000, easing = EaseInOut)
-//                )
-//                animatableY.animateTo(
-//                    targetValue = -100f,
-//                    animationSpec = tween(durationMillis = 3000, easing = EaseInOut)
-//                )
-//            }
-//
-//            launch {
-//                animatableRotation.animateTo(
-//                    targetValue = 360f,
-//                    animationSpec = tween(durationMillis = 3000, easing = EaseInOut)
-//                )
-//                animatableRotation.animateTo(
-//                    targetValue = 2000f,
-//                    animationSpec = tween(durationMillis = 3000, easing = EaseInOut)
-//                )
-//            }
-//        }
-//    }
-
     fun increaseWaterLevel(amt: Int) {
         if (amt < 0) throw Exception("Cannot Increase Water By Negative Value");
-
-        val willSurpassLimit = uiState.value.dailyWaterConsumedML + amt >= uiState.value.curDailyMaxWaterConsumedML;
-        val hasSurpassedLimit = uiState.value.dailyWaterConsumedML >= uiState.value.curDailyMaxWaterConsumedML;
-
-        if (willSurpassLimit && !hasSurpassedLimit) {
-            val newList = uiState.value.fishTypeList
-            newList.add(FishType.FISH_V1);
-            _uiState.update { currentState ->
-                currentState.copy(
-                    dailyWaterConsumedML = amt + currentState.dailyWaterConsumedML,
-                    fishDistances = generateSequentialFloatList(uiState.value.fishTypeList.size, 100f..600f),
-                    fishTypeList = newList
-                )
-            }
-        } else {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    dailyWaterConsumedML = amt + currentState.dailyWaterConsumedML,
-                )
-            }
+        _uiState.update { currentState ->
+            currentState.copy(
+                dailyWaterConsumedML = amt + currentState.dailyWaterConsumedML,
+            )
         }
     }
 }
--- app/src/main/java/com/hydrofish/app/ui/HydroFishUIState.kt
+++ app/src/main/java/com/hydrofish/app/ui/HydroFishUIState.kt
@@ -6,9 +6,7 @@ import androidx.compose.animation.core.AnimationVector1D
 data class HydroFishUIState(
     val dailyWaterConsumedML: Int = 0,
     val curDailyMaxWaterConsumedML: Int = 1000,
-    val fishTypeList: MutableList<FishType> = mutableListOf(FishType.FISH_V1, FishType.FISH_V1),
-    var fishDistances: List<Float> = listOf(300f, 500f),
 
-    var animatableX: Animatable<Float, AnimationVector1D> = Animatable(0f)
+    var fishScore: Int = 15,
 )
 
